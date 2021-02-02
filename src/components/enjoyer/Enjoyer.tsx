192abc1af31e9236c7c5d06951ce49d54907034d
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-01 18:53:22 -0800

    basic setup

---
+++ src/components/enjoyer/Enjoyer.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Enjoyer: React.FC<{ content: string }> = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Enjoyer;
