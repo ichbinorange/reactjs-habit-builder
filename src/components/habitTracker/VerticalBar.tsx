04796c28d6c210fa3a7eb4b0e83275a35bc4e666
Ting-Yi Liu <tingyi@Ting-Yis-MacBook-Pro.local>
2021-02-10 14:47:59 -0800

    drafted verticalbar to render month data for single habit correctly

--- src/components/habitTracker/VerticalBar.tsx
+++ src/components/habitTracker/VerticalBar.tsx
@@ -18,7 +18,7 @@ type stateType = {
 }
 
 const MONTH_LIST: Array<string> = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-const DAY_LIST: Array<number> = Array.from(Array(32).keys()).slice(1,32)
+// const DAY_LIST: Array<number> = Array.from(Array(32).keys()).slice(1,32)
 
 const VerticalBar: React.FC<stateType> = (props) => {
     const [habitTrackerData, setHabitTrackerData] = useState<Object[]>([]);
@@ -29,7 +29,7 @@ const VerticalBar: React.FC<stateType> = (props) => {
       return new Date(new Date(date).getFullYear(), new Date(date).getMonth() + 1, 0).getDate()
     }
     
-    // retrieve this month's data
+    // retrieve this month's data (t/f)
     const getThisMonth = (date: string) => {
         return new Date(date).getMonth() === new Date().getMonth()
     }
@@ -53,17 +53,25 @@ const VerticalBar: React.FC<stateType> = (props) => {
         });
     }, []); 
 
-    let habitData: VerticalBarSeriesPoint[] = new Array(daysPerMonth(`${new Date()}`));
-    const filterHabitComponents = habitTrackerData.filter((habitTracker: any) => (habitTracker.habit.id === props.habitId && getThisMonth(habitTracker.createdDate))).map((filteredHabitTracker: any) => {
-        let index: number = getDate(filteredHabitTracker.createdDate);
-        if (habitData[index] == undefined) {
-            habitData[index] = { x: (index + 1), y: filteredHabitTracker.workTime }
-        } else {
-            habitData[index].y += filteredHabitTracker.workTime 
+    const convertMonthData = () => {
+        let habitData: VerticalBarSeriesPoint[] = new Array(daysPerMonth(`${new Date()}`));
+        for (let i = 0; i < habitData.length; i++) {
+            if (habitData[i] == undefined) {
+                habitData[i] = { x: (i + 1), y: 0 }
+            }
         }
+
+        const filterHabitComponents = habitTrackerData.filter((habitTracker: any) => (habitTracker.habit.id === props.habitId && getThisMonth(habitTracker.createdDate))).map((filteredHabitTracker: any) => {
+            let index: number = getDate(filteredHabitTracker.createdDate) - 1;
+            if (habitData[index] == undefined) {
+                habitData[index] = { x: (index + 1), y: filteredHabitTracker.workTime }
+            } else {
+                habitData[index].y += filteredHabitTracker.workTime 
+            }
+          }
+        )
         return habitData
-      }
-    )
+    }
 
     // const habitTrackerComponents = habitTrackerData.map((habitTracker: any) => {
     // })
@@ -78,117 +86,36 @@ const VerticalBar: React.FC<stateType> = (props) => {
                     title: 'Habit#1',
                     color: '#19CDD7'
                 },
-                {
-                    title: 'Habit#1',
-                    color: '#DDB27C'
-                },
-                {
-                    title: 'Habit#3',
-                    color: '#88572C'
-                }
+                // {
+                //     title: 'Habit#1',
+                //     color: '#DDB27C'
+                // },
+                // {
+                //     title: 'Habit#3',
+                //     color: '#88572C'
+                // }
                 ]}
             />
             <XYPlot stackBy="y"
-                    xDomain={[0, 8]}
+                    xDomain={[0, daysPerMonth(`${new Date()}`)]}
                     yDomain={[0, 12]} 
                     width={500}
                     height={300}>
-                <VerticalGridLines />
                 <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
+                <XAxis attr="x"
+                        attrAxis="y"
+                        orientation="bottom"
+                        title={`day (${MONTH_LIST[new Date().getMonth()]}, ${new Date().getFullYear()})`}/>
+                <YAxis attr="y"
+                        attrAxis="x"
+                        orientation="left"
+                        title="spending time (hr)"/>
                 <VerticalBarSeries
                     color='#19CDD7'
                     barWidth={0.4}
-                    data={filterHabitComponents}
+                    data={convertMonthData()}
                     style={{}}
                 />
-                {/* <VerticalBarSeries
-                    barWidth={0.4}
-                    color='#DDB27C'
-                    data={[
-                    {
-                        x: 0,
-                        y: 10
-                    },
-                    {
-                        x: 1,
-                        y: 9.465822407644225
-                    },
-                    {
-                        x: 2,
-                        y: 8.058164550777553
-                    },
-                    {
-                        x: 3,
-                        y: 6.8148122483295195
-                    },
-                    {
-                        x: 4,
-                        y: 6.281585633203402
-                    },
-                    {
-                        x: 5,
-                        y: 7.115838957652743
-                    },
-                    {
-                        x: 6,
-                        y: 7.776259783194436
-                    },
-                    {
-                        x: 7,
-                        y: 8.022734318458077
-                    },
-                    {
-                        x: 8,
-                        y: 9.4359862665724
-                    }
-                    ]}
-                    style={{}}
-                />
-                <VerticalBarSeries
-                    barWidth={0.4}
-                    color='#88572C'
-                    data={[
-                    {
-                        x: 0,
-                        y: 10
-                    },
-                    {
-                        x: 1,
-                        y: 9.078216969430041
-                    },
-                    {
-                        x: 2,
-                        y: 8.541108080270531
-                    },
-                    {
-                        x: 3,
-                        y: 8.81957939963016
-                    },
-                    {
-                        x: 4,
-                        y: 8.862428204523804
-                    },
-                    {
-                        x: 5,
-                        y: 7.488062709992124
-                    },
-                    {
-                        x: 6,
-                        y: 7.797321036314394
-                    },
-                    {
-                        x: 7,
-                        y: 8.144588432131005
-                    },
-                    {
-                        x: 8,
-                        y: 9.289698776070031
-                    }
-                    ]}
-                    style={{}}
-                /> */}
             </XYPlot>
         </div>
     );
