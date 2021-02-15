12a55e29757718e0d96e5323b9b623265fa084f8
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-15 01:20:55 -0800

    fixed carousel seq

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -10,20 +10,20 @@ const Home: React.FC = () => {
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
             <div className="carousel-inner">
-                <div className="carousel-item">
-                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1551632811-561732d1e306?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="Third slide" />
+                <div className="carousel-item active">
+                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1551632811-561732d1e306?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="Independent backpacker?" />
                     <div className="carousel-caption d-none d-md-block">
                         <h4>Independent backpacker?</h4>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1580894908361-967195033215?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Second slide" />
+                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1580894908361-967195033215?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Build a mobile app in half a year?" />
                     <div className="carousel-caption d-none d-md-block">
                         <h4>Build a mobile app in half a year?</h4>
                     </div>
                 </div>
-                <div className="carousel-item active">
-                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1605522561233-768ad7a8fabf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80" alt="First slide" />
+                <div className="carousel-item">
+                    <img className="d-block w-100 img-fluid img-thumbnail" src="https://images.unsplash.com/photo-1605522561233-768ad7a8fabf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80" alt="Cook delicious Taiwanese food?" />
                     <div className="carousel-caption d-none d-md-block">
                         <h4>Cook delicious Taiwanese food?</h4>
                     </div>
