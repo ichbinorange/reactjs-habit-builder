7eaabe8bb8ce68d6d0432e54ab02020eb2897b61
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-14 23:10:54 -0800

    revised url to .github.io

--- src/components/util/BaseUrl.tsx
+++ src/components/util/BaseUrl.tsx
@@ -1,10 +1,15 @@
-export const API_BASE_URL = 'http://localhost:3000';
+// local
+// export const API_BASE_URL = 'http://localhost:3000';
 
 // For deployed server
-// export const API_BASE_URL = 'https://habit-builder-ada.herokuapp.com';
+export const API_BASE_URL = 'https://habit-builder-ada.herokuapp.com';
 
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3001/oauth2/redirect'
+// local
+// export const OAUTH2_REDIRECT_URI = 'http://localhost:3001/oauth2/redirect'
+
+// deployed front-end
+export const OAUTH2_REDIRECT_URI = 'https://ichbinorange.github.io/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
