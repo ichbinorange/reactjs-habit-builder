130160221db71bfaba86b1464e419d41988d2c6b
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-15 14:21:14 -0800

    moved delete button to card-footer for msgcard and habittrackercard

--- src/components/habitTracker/HabitTrackerCard.tsx
+++ src/components/habitTracker/HabitTrackerCard.tsx
@@ -24,16 +24,18 @@ const HabitTrackerCard: React.FC<stateType> = (props) => {
             Time spent(hr): {props.workTime}
             <br/>Note: {props.memo}
         </div>
-        <div className="text-right p-2">
-          <button
+      </div>
+      <div className="card-footer d-flex justify-content-between p-0 mt-2">
+        <div>
+          {(new Date(props.createdDate)).toLocaleString('en-US', DATE_OPTIONS)}
+        </div>
+        <button
               onClick={() => props.deleteHabitTrackerCallback(props.id)}
               className="btn btn-outline-danger btn-sm">
-              Delete
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
           </button>
-        </div>
-      </div>
-      <div className="card-footer text-center p-0">
-        {(new Date(props.createdDate)).toLocaleString('en-US', DATE_OPTIONS)}
       </div>
     </div>
   )
