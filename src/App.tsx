19f9ad0e90ff541303b8c1697d4a0a6d42a69367
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-02-15 00:14:11 -0800

    base /

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = (props) => {
   }, []);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       {loading ? <LoadingIndicator /> : null }
       <div className="content">
         <div className="app-top-box">
--- src/components/util/BaseUrl.tsx
+++ src/components/util/BaseUrl.tsx
@@ -10,6 +10,6 @@ export const ACCESS_TOKEN = 'accessToken';
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3001/oauth2/redirect'
 
 // deployed front-end
-export const OAUTH2_REDIRECT_URI = 'https://ichbinorange.github.io/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://ichbinorange.github.io/reactjs-habit-builder/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
